ba353fc0d3de435f45a90ce11fb9d8bd005a487e
KwiecienMarcin <47104423+KwiecienMarcin@users.noreply.github.com>
2025-06-10 15:20:49 +0200

    Update backend.js

--- backend.js
+++ backend.js
@@ -40,7 +40,7 @@ app.get('/score', async (req, res) => {
             raceTo: $(tds[3]).text().trim(),
             player1: cleanPlayerName($(tds[4])),
             flag1: getFullFlagUrl($(tds[5]).find('img').attr('src')),
-            score1: '7',
+            score1: '1',
             score2: $(tds[8]).text().trim(),
             flag2: getFullFlagUrl($(tds[8]).find('img').attr('src'))
               || getFullFlagUrl($(tds[9]).find('img').attr('src'))
