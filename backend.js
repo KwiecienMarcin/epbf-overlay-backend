1d3485a4b5bc6ae4dc0cbda78a7dc4d86991a7e2
KwiecienMarcin <47104423+KwiecienMarcin@users.noreply.github.com>
2025-06-11 02:25:08 +0200

    Update backend.js

--- backend.js
+++ backend.js
@@ -53,7 +53,7 @@ app.get('/score', async (req, res) => {
 
           const player1 = cleanPlayerName(p1Cell);
           const player2 = cleanPlayerName(p2Cell);
-          const score1 = '9';
+          const score1 = '7';
           const score2 = '7';
           const raceTo = $(tds[3]).text().trim();
           const table = $(tds[11]).text().trim();
